e2f3267018f2690a8959650226b5f6d8d4e855aa
JupiterPi <jupiterpi@outlook.de>
2019-02-10 16:36:59 +0100

    corrigated imports

--- src/main/java/jupiterpi/chatsubstandard/model/UserMapRepo.java
+++ src/main/java/jupiterpi/chatsubstandard/model/UserMapRepo.java
@@ -1,6 +1,8 @@
 package jupiterpi.chatsubstandard.model;
 
 import jupiterpi.chatsubstandard.model.*;
+import java.util.Map;
+import java.util.HashMap;
 
 class UserMapRepo implements UserRepo {
     Map<String,User> repo = new HashMap<String, User>();

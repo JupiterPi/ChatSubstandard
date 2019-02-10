1a06339fb13880d2144e6b7dc2304b1bfae3ac0b
ChristianBKraus <Christian.b.kraus@Web.de>
2019-02-10 16:45:45 +0100

    Message

---
+++ src/main/java/jupiterpi/chatsubstandard/model/Message.java
@@ -0,0 +1,11 @@
+package jupiterpi.chatsubstandard.model;
+
+import java.util.Date;
+
+public class Message 
+{
+	private String id;
+	private User sender;
+	private String message;
+	private Date time;
+}

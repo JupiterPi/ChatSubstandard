9e522c9bfd0e0e6e52d04a798cb81b56d0280b93
ChristianBKraus <Christian.b.kraus@Web.de>
2019-02-10 17:07:10 +0100

    getter setter

--- src/main/java/jupiterpi/chatsubstandard/model/Chatroom.java
+++ src/main/java/jupiterpi/chatsubstandard/model/Chatroom.java
@@ -6,4 +6,29 @@ public class Chatroom {
 	private boolean privateRoom;
 	private List<Message> messages = new ArrayList<Message>();
 	private List<User> users = new ArrayList<User>();
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public boolean isPrivateRoom() {
+		return privateRoom;
+	}
+	public void setPrivateRoom(boolean privateRoom) {
+		this.privateRoom = privateRoom;
+	}
+	public List<Message> getMessages() {
+		return messages;
+	}
+	public void setMessages(List<Message> messages) {
+		this.messages = messages;
+	}
+	public List<User> getUsers() {
+		return users;
+	}
+	public void setUsers(List<User> users) {
+		this.users = users;
+	}
 }

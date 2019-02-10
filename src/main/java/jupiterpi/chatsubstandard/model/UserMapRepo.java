f3b30f42a65342805f02f7b2ab6ad875e03abc04
ChristianBKraus <Christian.b.kraus@Web.de>
2019-02-10 16:39:27 +0100

    Kleine Projectänderungen und korrektur UserRepo

--- src/main/java/jupiterpi/chatsubstandard/model/UserMapRepo.java
+++ src/main/java/jupiterpi/chatsubstandard/model/UserMapRepo.java
@@ -1,12 +1,11 @@
 package jupiterpi.chatsubstandard.model;
 
-import jupiterpi.chatsubstandard.model.*;
 import java.util.Map;
 import java.util.HashMap;
 
 class UserMapRepo implements UserRepo {
     Map<String,User> repo = new HashMap<String, User>();
 	public User findByUsername(String username) {
-		repo.get(username);
+		return repo.get(username);
 	}
 }

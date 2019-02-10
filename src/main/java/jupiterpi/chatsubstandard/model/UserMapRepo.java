8e110c1d434b4e79390916e542b444ad7e31cbb6
JupiterPi <jupiterpi@outlook.de>
2019-02-10 16:38:28 +0100

    corrigated something

--- src/main/java/jupiterpi/chatsubstandard/model/UserMapRepo.java
+++ src/main/java/jupiterpi/chatsubstandard/model/UserMapRepo.java
@@ -7,6 +7,6 @@ import java.util.HashMap;
 class UserMapRepo implements UserRepo {
     Map<String,User> repo = new HashMap<String, User>();
 	public User findByUsername(String username) {
-		repo.get(username);
+		return repo.get(username);
 	}
 }

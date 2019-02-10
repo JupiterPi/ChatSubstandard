3dff8f42675e91f4d746e9a31a4a4b1f144afcf0
JupiterPi <jupiterpi@outlook.de>
2019-02-10 16:29:36 +0100

    basics

---
+++ src/main/java/jupiterpi/chatsubstandard/ChatSubstandard.java
@@ -0,0 +1,9 @@
+package jupiterpi.chatsubstandard;
+
+public class ChatSubstandard
+{
+    public static void main (String[] args)
+    {
+        System.out.println ("ALAAAAARMMMMM !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! Das ist nur ein Probealarm!!!");
+    }
+}
---
+++ src/main/java/jupiterpi/chatsubstandard/User.java
@@ -0,0 +1,9 @@
+package jupiterpi.chatsubstandard;
+
+public class User
+{
+    private String name;
+    private String username;
+    private String password;
+    private boolean isAdmin;
+}

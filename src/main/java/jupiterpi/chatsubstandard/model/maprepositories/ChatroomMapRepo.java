9d21916042c561c627335e095aad6885e4f696ce
ChristianBKraus <Christian.b.kraus@Web.de>
2019-02-10 17:15:35 +0100

    correction import

--- src/main/java/jupiterpi/chatsubstandard/model/maprepositories/ChatroomMapRepo.java
+++ src/main/java/jupiterpi/chatsubstandard/model/maprepositories/ChatroomMapRepo.java
@@ -2,6 +2,7 @@ package jupiterpi.chatsubstandard.model.maprepositories;
 
 import java.util.*;
 import jupiterpi.chatsubstandard.model.*;
+import jupiterpi.chatsubstandard.model.repositories.*;
 
 public class ChatroomMapRepo implements ChatroomRepo{
 	Map<String,Chatroom> repo = new HashMap<String,Chatroom>();
